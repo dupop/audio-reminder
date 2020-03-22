8b4d4ac3849d45011919d6319ebff09d2799ca15
dupop <62511308+dupop@users.noreply.github.com>
2020-03-22 20:58:05 +0100

    Added serilog; Extracted HotKeyListener class; WIP

--- AudioReminder/AudioReminderService.cs
+++ AudioReminder/AudioReminderService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GlobalHotKey;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,12 +7,16 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace AudioReminder
 {
     public partial class AudioReminderService : ServiceBase
     {
+        HotKeyListener hotKeyListener;
         public AudioReminderService()
         {
             InitializeComponent();
@@ -19,10 +24,14 @@ namespace AudioReminder
 
         protected override void OnStart(string[] args)
         {
+            hotKeyListener = new HotKeyListener();
         }
 
+       
         protected override void OnStop()
         {
         }
+
+       
     }
 }
---
+++ AudioReminder/HotKeyListener.cs
@@ -0,0 +1,47 @@
+﻿using GlobalHotKey;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace AudioReminder
+{
+    class HotKeyListener
+    {
+
+        Thread hotKeyListeningThread;
+
+        HotKeyManager hotKeyManager;
+        HotKey hotKey;
+
+        public HotKeyListener()
+        {
+            hotKeyListeningThread = new Thread(new ThreadStart(StartHotKeyListener));
+            hotKeyListeningThread.SetApartmentState(ApartmentState.STA);
+            StartHotKeyListener();
+        }
+
+        private void StartHotKeyListener()
+        {
+            HotKeyManager hotKeyManager = new HotKeyManager();
+            hotKeyManager.KeyPressed += HotKeyManagerPressed;
+            hotKey = hotKeyManager.Register(Key.F5, ModifierKeys.Control);
+        }
+
+        private void StopListener()
+        {
+            hotKeyManager.Unregister(hotKey);
+            hotKeyManager.Dispose();
+        }
+
+        private void HotKeyManagerPressed(object sender, KeyPressedEventArgs e)
+        {
+            if (e.HotKey.Key == Key.F5)
+                MessageBox.Show("Hot key pressed!");
+        }
+    }
+}
