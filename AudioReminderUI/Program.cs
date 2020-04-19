616904b54a6c350f27423ec424396a049b1a0ae1
dupop <62511308+dupop@users.noreply.github.com>
2020-04-19 23:03:17 +0200

    Beeper moments are now relative to midnight, not to moment of service starting

--- AudioReminderUI/Program.cs
+++ AudioReminderUI/Program.cs
@@ -10,6 +10,7 @@ namespace AudioReminderUI
     //TODO: make master default branch again or keep develop as default?
     //TODO: DP->SI add help button and add there thanks to Rale & test that with NVDA free narator program from internet
     //TODO: creating (remote?) backup of reminders file, and "exporting" feature
+    //TODO: Useful feature - time until next reminder on UI
 
     static class Program
     {
